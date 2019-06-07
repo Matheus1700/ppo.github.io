5257f90bdca0096c0b52364d5c81fbcfe30b2ffc
SAMUEL DE SOUZA FERRAZ <20151D12GR0251@LABS.GARANHUNS.IFPE>
2019-06-07 09:53:51 -0300

    Teste audio

--- PPO/telaInicial.js
+++ PPO/telaInicial.js
@@ -8,13 +8,20 @@ constructor(){
 preload(){
     this.load.image("fundo", "telaInicial.png");
     this.load.image("start", "start.png");
+
+    this.load.audio("botaoClick", "audio/correctSound.mp3");
 } 
     
 create() {
     this.add.image(490, 338, "fundo");
     this.helloButton = this.add.image(490, 390, "start")
-    .setInteractive().on('pointerdown', () => { this.scene.start("jogo") });
+    .setInteractive().on('pointerdown', () => { 
+        let soundSample = this.sound.add("botaoClick");
+        soundSample.play();
+        this.scene.start("jogo") });
 
+
+    
   }
   
 }
