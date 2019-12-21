89c4c39facb69e0447039729311827187bbebcad
thsmmil <44467854+thsmmil@users.noreply.github.com>
2019-12-21 18:54:29 -0300

    Add files via upload

--- PPO/telaInicial.js
+++ PPO/telaInicial.js
@@ -7,7 +7,10 @@ constructor(){
 
 preload(){
 
-    this.load.image("fundo", "telaInicial.png");
+    this.load.image("fundo", "img/telaCriarSala.jpg");
+    
+    this.load.image("botao", "img/botao.jpg");
+    this.load.image("logo", "img/logoRefeitaLaranja.png");
 
     this.load.audio("botaoClick", "audio/correctSound.mp3");
     this.load.audio("themeSong", "audio/FaketalesofSanFrancisko.wav");
@@ -19,15 +22,18 @@ preload(){
 } 
     
 create() {
-
-
+ 
     var audioConfig={
         delay: 0,
         volume: 1,
         loop: true
     }
 
-
+    //Aqui ta criando o elemento INPUT no JS usando a tag 'input1'
+    // que já tinha sido criado no telaInicialHtml
+    var inputUm = document.createElement("input1");
+    var inputDois = document.createElement("input2");
+    
     let soundSample = this.sound.add("botaoClick")
     let soundTrack = this.sound.add("themeSong")
     let config = this.cache.json.get('gamma_json');
@@ -36,13 +42,31 @@ create() {
 
     this.cache.bitmapFont.add('gamma', Phaser.GameObjects.RetroFont.Parse(this, config));
 
+    
     this.add.image(490, 338, "fundo");
+    
+    let logo = this.add.image(470, 180, "logo");
+    logo.setScale(0.55);
+    
+    this.texto1 = this.add.bitmapText(135, 320, "gamma", 'NOME');
+    this.texto1.setScale(1.3);
 
-    this.botaoStart = this.add.bitmapText(388, 400, "gamma", 'START GAME')
-    .setInteractive().on('pointerdown', () => { this.scene.start("jogo")
-    soundSample.play(); soundTrack.stop(); });
+    this.texto2 = this.add.bitmapText(135, 394, "gamma", 'SENHA');
+    this.texto2.setScale(1.3);
 
-    this.botaoStart.setFontSize(22);
+    
+    let botaoIniciar = this.add.image(475, 537, "botao").
+    setInteractive().on('pointerdown', () => { this.scene.start("menuPrincipal",
+    document.getElementById("input1").style.display = "none",
+    document.getElementById("input2").style.display = "none"   
+    )
+}
+);;
+    
+    
+    
+
+  
 
 }
 
