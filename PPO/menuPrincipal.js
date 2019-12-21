58340e14cf6ee9d2127d1afa555ded10e865b5b2
thsmmil <44467854+thsmmil@users.noreply.github.com>
2019-12-21 18:53:53 -0300

    Add files via upload

---
+++ PPO/menuPrincipal.js
@@ -0,0 +1,77 @@
+class menuPrincipal extends Phaser.Scene{
+
+    constructor(){
+        super("menuPrincipal");
+    }
+
+    preload(){
+        this.load.image("fundo2", "img/telaNormal.jpg");
+        this.load.image("fundo3", "img/telaSimples.jpg");
+
+        this.load.image("botaoCriar1", "img/botaoCriar.png");
+        this.load.image("botaoEstatistica1", "img/botaoEstatistica.png");
+        this.load.image("botaoFechar1", "img/botaoFechar.png");
+        this.load.image("botaoIniciar1", "img/botaoIniciar.png");
+
+        this.load.path = './fontes/';
+        this.load.image('gamma', 'font.png');
+        this.load.json('gamma_json', 'gamma.json');
+
+    }
+
+    create(){
+        var x = this.add.image(490, 338, "fundo2");
+        x.setScale(0.75);
+        
+        this.add.dom(490, 338, 'div', 'background-color: white; width: 220px; height: 100px; font: 48px Arial', 'Phaser');
+
+
+        let botaoCriar = this.add.image(664.5, 270, "botaoCriar1").setScale(0.60).
+        setInteractive().on('pointerdown', () => { this.scene.start("criarPartida")});
+
+        let botaoEstatistica = this.add.image(664.5, 430, "botaoEstatistica1").setScale(0.6)
+        .setInteractive().on('pointerdown', () => { this.scene.start("estatisticas")});
+
+        let botaoIniciar = this.add.image(664.5, 645, "botaoIniciar1").setScale(0.6)
+        .setInteractive().on('pointerdown', () => { this.scene.start("entrarPartida")});
+
+        let botaoFechar = this.add.image(664.5, 595, "botaoFechar1").setScale(0.6)
+        .setInteractive().on('pointerdown', () => { this.scene.start("telaInicial")});
+
+       
+        let config = this.cache.json.get('gamma_json');
+        this.cache.bitmapFont.add('gamma', Phaser.GameObjects.RetroFont.Parse(this, config));
+
+        this.texto1 = this.add.bitmapText(450, 214, "gamma", 'CRIAR PARTIDA');
+        this.texto1.setScale(1.5);
+
+        this.texto2 = this.add.bitmapText(452, 296, "gamma", 'ESTATISTICAS');
+        this.texto2.setScale(1.5);
+
+        this.texto3 = this.add.bitmapText(452, 376, "gamma", 'BUSCAR PARTIDA');
+        this.texto3.setScale(1.5);
+
+        this.texto4 = this.add.bitmapText(452, 461, "gamma", 'SAIR');
+        this.texto4.setScale(1.5);
+       
+        this.textoMenu = this.add.bitmapText(10, 40, "gamma", 'MENU PRINCIPAL');
+        this.textoMenu.setScale(2);
+
+
+
+        botaoCriar.setInteractive();    
+        
+        botaoCriar.on("pointerover", ()=>{
+            botaoCriar = (664.5, 270, "botaoCriar2");
+            console.log("pointerOver funciona e muito bem :)");
+        })
+
+        botaoCriar.on("pointerout", ()=>{
+            botaoCriar = ("botaoCriar1");
+            console.log("pointerOut funciona e muito bem :)");
+
+        })
+
+   
+    }
+}
